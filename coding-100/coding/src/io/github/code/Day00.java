e87509f3e8398b1993edbc9ae0d81c0bd6ab8590
manjaro <manjaro@ctemplar.com>
2019-11-02 17:13:53 +0800

    Day-02

--- coding-100/coding/src/io/github/code/Day00.java
+++ coding-100/coding/src/io/github/code/Day00.java
@@ -6,11 +6,12 @@ import java.util.List;
 
 /**
  * 【顺时针螺旋矩阵】 给定一个包含 m x n 个元素的矩阵（m 行 n 列） 按照顺时针螺旋顺序，返回矩阵中的所有元素
- * 【示例】 输入：
- * [ [1,2,3], [4,5,6], [7,8,9] ]
- * 输出： [1,2,3,6,9,8,7,4,5]
+ * 【示例输入：】
+ *  [ [1,2,3], [4,5,6], [7,8,9] ]
+ * 【示例输出：】
+ *  [1,2,3,6,9,8,7,4,5]
  */
-public class coding00 {
+public class Day00 {
     public static List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
         int height = matrix.length;
